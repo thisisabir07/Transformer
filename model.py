45e35c48e85603d5b26e7b53ef73b22f68206a11
thisisabir07 <abirsc7@gmail.com>
2024-10-27 14:38:09 +0530

    Decoder completed

--- model.py
+++ model.py
@@ -173,3 +173,31 @@ class Encoder(nn.Module):
             x = layer(x, mask)
         return self.norm(x)
 
+
+class DecoderBlock(nn.Module):
+
+    def __init__(self, self_attention_block: MultiHeadAttentionBlock, cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection = nn.ModuleList([ResidualConnection(dropout) for _ in range(3)])
+
+    def forward(self, x, encoder_output, source_mask, target_mask):
+        x = self.residual_connection[0](x, lambda x: self.self_attention_block(x, x, x, target_mask))
+        x = self.residual_connection[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, source_mask))
+        x = self.residual_connection[2](x, self.feed_forward_block)
+        return x
+
+class Decoder(nn.Module):
+
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+
+    def forward(self, x, encoder_output, source_mask, target_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, source_mask, target_mask)
+        return self.norm(x)
+
